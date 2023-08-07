da28cd75481a4d263f1a0753cf0669b57c0d2731
Sait-Boz <sait.bozteke63@gmail.com>
2023-08-07 12:04:58 +0200

    file eklendi

--- src/components/SearchMovieCard.js
+++ src/components/SearchMovieCard.js
@@ -7,30 +7,31 @@ const defaultImage =
   "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
 const MovieCard = (searchMovie) => {
-  console.log("searchMovies", searchMovie.Poster);
+  console.log("searchMovies", searchMovie);
   const { currentUser } = useContext(AuthContext);
   let navigate = useNavigate();
   return (
-    <div
-      className="movie"
-      onClick={() =>
-        currentUser ? navigate("details/" + searchMovie.imbdId) : alert("login")
-      }
-    >
-      <img
-        src={searchMovie.Poster ? searchMovie.Poster : defaultImage}
-        alt=""
-      />
-      <div className="d-flex align-items-baseline justify-content-between p-1 text-white">
-        <h5>{searchMovie.Title}</h5>
-        {currentUser && <span>{searchMovie.Year}</span>}
-      </div>
+    <>
+      {Object.keys(searchMovie).map((movie) => (
+        <div
+          className="movie"
+          onClick={() =>
+            currentUser ? navigate("details/" + movie.imbdId) : alert("login")
+          }
+        >
+          <img src={movie.Poster ? movie.Poster : defaultImage} alt="" />
+          <div className="d-flex align-items-baseline justify-content-between p-1 text-white">
+            <h5>{movie.Title}</h5>
+            {currentUser && <span>{movie.Year}</span>}
+          </div>
 
-      <div className="movie-over">
-        <h2>Overview</h2>
-        <p>{searchMovie.Type}</p>
-      </div>
-    </div>
+          <div className="movie-over">
+            <h2>Overview</h2>
+            <p>{movie.Type}</p>
+          </div>
+        </div>
+      ))}
+    </>
   );
 };
 
